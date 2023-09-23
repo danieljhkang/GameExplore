b6ea99e246b96e2f656cc00b53f23df9c7d6882c
Daniel Kang <dkang.jh@gmail.com>
2023-09-22 20:25:24 -0400

    built sort functionality

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,6 @@
 
 const getCroppedImageURL = (url:string) => {
+    if (!url) return '';
     const target = 'media/';
     const index = url.indexOf(target) + target.length; //index represents starting position of media parameter
     return url.slice(0,index) + 'crop/600/400/' + url.slice(index);
