c02fd47f94bca214426fd1594c6ab26c158d0d64
Daniel Kang <dkang.jh@gmail.com>
2023-09-23 21:00:14 -0400

    added env variables

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL : 'https://api.rawg.io/api',
     params: {
-        key: '55f72ecbb25a4d86a2d66eb3557937e0'
+        key: import.meta.env.VITE_KEY
     }
 })
